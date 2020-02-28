e47ea8f01b5b6cf358426971a5c1eab53566f693
JanaFlauschata <jana.kutscheid@gmail.com>
2020-02-28 15:19:46 +0100

    :zap: As a user I can add a recipe with a picture from the camera #4 Testing Picasso for image loading (also adding RecipeImageService).

--- app/src/main/java/org/flauschhaus/broccoli/recipes/images/RecipeImageService.java
+++ app/src/main/java/org/flauschhaus/broccoli/recipes/images/RecipeImageService.java
@@ -34,6 +34,30 @@ public class RecipeImageService {
         return BitmapFactory.decodeFile(imageDirectory.getAbsolutePath() + "/" + imageName);
     }
 
+    File getFile(String imageName) {
+        File imageDirectory = getImageDirectory();
+        return new File(imageDirectory.getAbsolutePath() + "/" + imageName);
+    }
+
+    Bitmap loadBitmapWithName(String imageName, int targetWidth, int targetHeight) {
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        int photoHeight = bmOptions.outWidth;
+        int imageHeight = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoHeight/targetWidth, imageHeight/targetHeight);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        File imageDirectory = getImageDirectory();
+        return BitmapFactory.decodeFile(imageDirectory.getAbsolutePath() + "/" + imageName, bmOptions);
+    }
+
     private File getImageDirectory() {
         return application.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
     }
