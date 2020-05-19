4054afcbcf53dd24407cc581fbae7a204d811d8d
JanaFlauschata <jana.kutscheid@gmail.com>
2020-05-19 17:03:08 +0200

    :white_check_mark: Trying to fix broken test on Firebase.

--- app/src/androidTest/java/org/flauschhaus/broccoli/ui/recipes/CreateAndEditRecipeActivityTest.java
+++ app/src/androidTest/java/org/flauschhaus/broccoli/ui/recipes/CreateAndEditRecipeActivityTest.java
@@ -38,6 +38,8 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.pressKey;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intending;
@@ -120,7 +122,7 @@ public class CreateAndEditRecipeActivityTest {
         onView(withId(R.id.new_servings)).perform(closeSoftKeyboard(), typeText(LAUCHKUCHEN.getServings()));
         onView(withId(R.id.new_preparation_time)).perform(closeSoftKeyboard(), typeText(LAUCHKUCHEN.getPreparationTime()));
         onView(withId(R.id.new_ingredients)).perform(closeSoftKeyboard(), typeText(LAUCHKUCHEN.getIngredients())); // it seems not to be possible to make Espresso type the enter key in a deterministic way
-        onView(withId(R.id.new_directions)).perform(closeSoftKeyboard(), typeText(LAUCHKUCHEN.getDirections()));
+        onView(withId(R.id.new_directions)).perform(closeSoftKeyboard(), swipeUp(), typeText(LAUCHKUCHEN.getDirections())); // trying out whether swiping works on Firebase devices
         onView(withId(R.id.button_save_recipe)).perform(click()); // TODO find out why there sometimes is such a long wait
 
         verify(recipeImageService).moveImage("12345.jpg");
