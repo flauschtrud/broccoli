16a3eb9b30778e2683502daca90784433da62cd8
JanaFlauschata <jana.kutscheid@gmail.com>
2020-05-26 08:56:48 +0200

    :white_check_mark: Another try to fix the test problems on Firebase.

--- app/src/main/java/org/flauschhaus/broccoli/ui/recipes/RecipeDetailsActivity.java
+++ app/src/main/java/org/flauschhaus/broccoli/ui/recipes/RecipeDetailsActivity.java
@@ -59,6 +59,7 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         binding.backdrop.setOnClickListener(view -> binding.appBar.setExpanded(true, true));
 
         setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Recipe recipe = (Recipe) getIntent().getSerializableExtra(Recipe.class.getName());
