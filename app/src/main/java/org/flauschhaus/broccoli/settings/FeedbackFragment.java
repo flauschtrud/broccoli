d7b928c7eaa10ca814dd86e6e098e35d162e9a0e
JanaFlauschata <jana.kutscheid@gmail.com>
2020-08-10 09:32:08 +0200

    :bug: As a user I can send feedback emails directly from the app #54 Used ACTION_SENDTO to limit apps for intent.

--- app/src/main/java/org/flauschhaus/broccoli/settings/FeedbackFragment.java
+++ app/src/main/java/org/flauschhaus/broccoli/settings/FeedbackFragment.java
@@ -1,6 +1,7 @@
 package org.flauschhaus.broccoli.settings;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -17,8 +18,8 @@ public class FeedbackFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/email");
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
         intent.putExtra(Intent.EXTRA_EMAIL, new String[]{ getString(R.string.feedback_email) });
         intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.feedback_subject));
         intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.feedback_text));
