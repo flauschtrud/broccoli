7b8e8f37685d159049d77bf1004683c3dcf53cb5
JanaFlauschata <jana.kutscheid@gmail.com>
2020-10-20 10:19:55 +0200

    :sparkles: As a user I can export my recipes and reimport them to an empty database including categories #73 Show a dialog before really restoring data.

--- app/src/main/java/org/flauschhaus/broccoli/backup/BackupAndRestoreFragment.java
+++ app/src/main/java/org/flauschhaus/broccoli/backup/BackupAndRestoreFragment.java
@@ -3,6 +3,7 @@ package org.flauschhaus.broccoli.backup;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -42,9 +43,15 @@ public class BackupAndRestoreFragment extends PreferenceFragmentCompat {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_ARCHIVE_GET && resultCode == RESULT_OK) {
-            Intent intent = new Intent(getActivity(), RestoreService.class);
-            intent.setData(data.getData());
-            RestoreService.enqueueWork(getContext(), intent);
+            new AlertDialog.Builder(getContext())
+                    .setTitle(R.string.restore)
+                    .setMessage(R.string.restore_alert_message)
+                    .setNeutralButton(R.string.cancel, (dialog, id) -> {})
+                    .setPositiveButton(R.string.restore_action, (dialog, id) -> {
+                        Intent intent = new Intent(getActivity(), RestoreService.class);
+                        intent.setData(data.getData());
+                        RestoreService.enqueueWork(getContext(), intent);
+                    }).show();
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
