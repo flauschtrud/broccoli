afa667d7f8c5c6fc1008d265f642ca4205499a56
flauschtrud <jana@flauschcode.com>
2021-07-12 13:57:00 +0200

    :sparkles: As an excessively curious user I can view all the licenses of the open source dependencies #149

--- app/src/main/java/com/flauschcode/broccoli/help/HelpAndFeedbackFragment.java
+++ app/src/main/java/com/flauschcode/broccoli/help/HelpAndFeedbackFragment.java
@@ -1,15 +1,26 @@
 package com.flauschcode.broccoli.help;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.flauschcode.broccoli.R;
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 
 public class HelpAndFeedbackFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.help_and_feedback, rootKey);
+
+        Preference licensesPreference = findPreference("oss-licenses");
+        if(licensesPreference != null){
+            licensesPreference.setOnPreferenceClickListener(preference -> {
+                startActivity(new Intent(requireActivity(), OssLicensesMenuActivity.class));
+                return true;
+            });
+        }
     }
 }
