2a07710e0f5ba823e512d8a7e595e618d9149298
flauschtrud <jana@flauschcode.com>
2024-12-10 12:52:07 +0100

    Additional Notes field under the recipe #228 Removed @FlakyTest annotation for certain tests.

--- app/src/androidTest/java/com/flauschcode/broccoli/CRUDIntegrationTest.java
+++ app/src/androidTest/java/com/flauschcode/broccoli/CRUDIntegrationTest.java
@@ -17,7 +17,6 @@ import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.accessibility.AccessibilityChecks;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.FlakyTest;
 
 import com.flauschcode.broccoli.util.RecyclerViewAssertions;
 import com.flauschcode.broccoli.util.RecyclerViewMatcher;
@@ -28,7 +27,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-@FlakyTest
 public class CRUDIntegrationTest {
 
     private ActivityScenario<MainActivity> scenario;
